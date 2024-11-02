bfa49a2536759c3c8d34a7d8ea9f1e8c642d314b
786haider <haiderhussain536@gmail.com>
2024-11-01 17:55:13 -0700

    second

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-     <div><h1>Hello</h1></div> 
+     <div className="flex justify-center bg-black text-6xl text-emerald-500">
+      <h1>Hello</h1>
+      <h2 className="bg-pink-500 text-black text-4xl"></h2>
+      </div> 
   );
 }
